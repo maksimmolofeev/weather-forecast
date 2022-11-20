5aeffeac77474750ef4ce2fb27dc349a92ba7055
maksimmolofeev <109696557+maksimmolofeev@users.noreply.github.com>
2022-11-20 09:32:23 +0300

    Second commit

--- index.js
+++ index.js
@@ -3,29 +3,14 @@ const btn = document.querySelector('.container__button');
 const outputCity = document.querySelector('.container__city-name');
 const degrees = document.querySelector('.container__degrees');
 const condition = document.querySelector('.container__condition-weather')
-const img = document.querySelector('.img')
+const img = document.querySelector('.img');
+const imgSearch = document.querySelector('.container__img-search');
+const citySearch = document.querySelector('.container__city-search');
+const cityChoice = document.querySelector('.container__city-choice');
+
 
 let cityId = 524901;
 weatherFetch(cityId);
-
-const cityList = [
-    {
-        name: 'Moscow',
-        id: 524901
-    },
-
-    {
-        name: 'Side',
-        id: 301238
-    },
-
-    {
-        name: 'Penza',
-        id: 519560
-    }
-];
-
-//https://openweathermap.org/img/wn/04d@2x.png
 
 async function weatherFetch(id) {
     const url = `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=e2c12abd7b2bb8d0875f2aa65538e020`;
@@ -42,16 +27,39 @@ async function weatherFetch(id) {
     }
 };
 
-select.addEventListener('click', () => {})
-
-btn.addEventListener('click', () => {
-    
-    let cityName = select.value;
+function getCheckElement (cityName) {
     cityList.forEach(el => {
-        if (el.name === cityName) {
+        if (el.name.toLowerCase() === cityName.toLowerCase()) {
             cityId = el.id;
         }
     });
 
     weatherFetch(cityId);
+}
+
+btn.addEventListener('click', () => {
+    
+    let cityName = select.value;
+
+    getCheckElement(cityName);
+});
+
+imgSearch.addEventListener('click', () => {
+    cityChoice.style.display = "none";
+    citySearch.style.display = "block";
+    citySearch.focus();
+    citySearch.onkeyup = (event) => {
+
+        if (event.keyCode === 13) {
+            let cityName = citySearch.value;
+
+            getCheckElement(cityName)
+
+            citySearch.style.display = "none";
+            cityChoice.style.display = "block";
+            citySearch.value = '';
+            cityChoice.value = cityName.charAt(0).toUpperCase() + cityName.slice(1)
+            console.log(select.value)
+        };
+    };
 });
